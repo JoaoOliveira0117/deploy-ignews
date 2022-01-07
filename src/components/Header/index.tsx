13e47a457ac3c070500e3c05e08e5e7a64f5feab
JoaoOliveira0117 <joao.oliveira0117@hotmail.com>
2022-01-07 19:48:03 -0300

    add eslint rules + fix lint issues on files

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,28 @@
-import {useRouter} from 'next/router';
-import { ActiveLink } from '../ActiveLink';
-import { SignInButton } from './SignInButton';
+import { useRouter } from "next/router";
+import Image from "next/image";
+import { ActiveLink } from "../ActiveLink";
+import { SignInButton } from "./SignInButton";
 
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
-export function Header(){
-    const { asPath } = useRouter();
+export function Header() {
+  const { asPath } = useRouter();
 
-    return (
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news" />
-                <nav>
-                    <ActiveLink activeClassName={styles.active} href="/">
-                        <a>Home</a>
-                    </ActiveLink>
-                    <ActiveLink activeClassName={styles.active} href="/posts">
-                        <a>Posts</a>
-                    </ActiveLink>
-                </nav>
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
+        </nav>
 
-                <SignInButton/>
-            </div>
-        </header>
-    );
+        <SignInButton />
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
-import { SubscribeButton } from '../components/SubscribeButton';
-import { stripe } from '../services/stripe';
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
-import styles from './home.module.scss';
+import styles from "./home.module.scss";
 
 interface HomeProps {
   product: {
     priceId: string;
     amount: number;
-  }
+  };
 }
 
 export default function Home({ product }: HomeProps) {
@@ -18,40 +19,43 @@ export default function Home({ product }: HomeProps) {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      
+
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, Welcome</span>
-          <h1> News about the <span>React</span> world.</h1>
+          <h1>
+            {" "}
+            News about the <span>React</span> world.
+          </h1>
           <p>
             Get access to all the publications <br />
             <span>for {product.amount} month.</span>
           </p>
-          <SubscribeButton priceId={product.priceId}/>
+          <SubscribeButton priceId={product.priceId} />
         </section>
-        <img src="/images/avatar.svg" alt="Girl Coding"></img>
+        <Image src="/images/avatar.svg" alt="Girl Coding"></Image>
       </main>
     </>
-  )
+  );
 }
 
-export const getStaticProps: GetStaticProps = async() => {
-  const price = await stripe.prices.retrieve('price_1JcJ3NDWB070oHRMJyFSqDY3', {
-    expand: ['product']
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JcJ3NDWB070oHRMJyFSqDY3", {
+    expand: ["product"],
   });
 
   const product = {
     priceId: price.id,
-    amount: new Intl.NumberFormat('en-US',{
-      style: 'currency',
-      currency: 'USD',
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
     }).format(price.unit_amount / 100),
   };
 
   return {
     props: {
-      product
+      product,
     },
     revalidate: 60 * 60 * 24, // 24 hours
-  }
-}
+  };
+};
